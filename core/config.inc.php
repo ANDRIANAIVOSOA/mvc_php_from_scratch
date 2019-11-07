9b544eb516d514c05b9aa6227e0f2ba984e2c4eb
ANDRIANAIVOSOA <fenitraandrianaivosoa@gmail.com>
2019-11-06 23:41:15 -0800

    structure

---
+++ core/config.inc.php
@@ -0,0 +1,14 @@
+{
+	"name": "collectify",
+	"licence": "MIT",
+	"type": "project",
+	"description": "outils d aministration d collectify",
+	"autoload": {
+		"psr-0": {
+			"":"src/"
+		}
+	},
+	"require":{
+		
+	}
+}
