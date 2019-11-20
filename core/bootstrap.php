4053f0c50d40cb61d34bd4c5c52df7b5663384e4
ANDRIANAIVOSOA <fenitraandrianaivosoa@gmail.com>
2019-11-20 11:14:10 +0300

    psr-4 et readBean ok

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 /**
  * configuration redbean
  */
-use ReadBean_Facade as R;
+use RedBeanPHP\Facade as R;
 if(empty(R::$currentDB)){
 	$dsn = sprintf('%s:host=%s;dbname=%s', DB_TYPE, DB_HOST, DB_NAME);
 	R::setup($dsn, DB_USER, DB_PASSWORD);
